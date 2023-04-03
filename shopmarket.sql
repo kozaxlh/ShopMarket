3de3e1f8a7a3e4197f15798654675da3704203c3
Admin <Admin@Admin-PC>
2023-04-03 10:41:03 +0700

    fixed database

--- shopmarket.sql
+++ shopmarket.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Aug 16, 2021 at 03:34 AM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- Host: 127.0.0.1
+-- Generation Time: Apr 03, 2023 at 05:39 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,26 +18,26 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Market`
+-- Database: `shopmarket`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Category`
+-- Table structure for table `category`
 --
 
-CREATE TABLE `Category` (
+CREATE TABLE `category` (
   `CatagoryID` int(10) NOT NULL,
   `Name` varchar(30) NOT NULL,
   `Description` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `Category`
+-- Dumping data for table `category`
 --
 
-INSERT INTO `Category` (`CatagoryID`, `Name`, `Description`) VALUES
+INSERT INTO `category` (`CatagoryID`, `Name`, `Description`) VALUES
 (1, 'Fruit', 'The kind that can be eaten without cooking'),
 (2, 'Green Vegetables', 'The kind used to make salads or soups'),
 (3, 'Spices', 'The kind used to enhance the taste of food');
@@ -46,10 +45,10 @@ INSERT INTO `Category` (`CatagoryID`, `Name`, `Description`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Customers`
+-- Table structure for table `customers`
 --
 
-CREATE TABLE `Customers` (
+CREATE TABLE `customers` (
   `CustomerID` int(10) NOT NULL,
   `Password` varchar(20) NOT NULL,
   `Fullname` varchar(40) NOT NULL,
@@ -58,10 +57,10 @@ CREATE TABLE `Customers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `Customers`
+-- Dumping data for table `customers`
 --
 
-INSERT INTO `Customers` (`CustomerID`, `Password`, `Fullname`, `Address`, `City`) VALUES
+INSERT INTO `customers` (`CustomerID`, `Password`, `Fullname`, `Address`, `City`) VALUES
 (1, 'Abcd1234', 'John Smith', '30 Broadway', 'London'),
 (2, 'Abcd1234', 'Jonny English', '99 River View', 'Reading'),
 (3, 'Abcd1234', 'Elizabeth', '23 Buckinghamshire', 'York'),
@@ -70,10 +69,10 @@ INSERT INTO `Customers` (`CustomerID`, `Password`, `Fullname`, `Address`, `City`
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Order`
+-- Table structure for table `order`
 --
 
-CREATE TABLE `Order` (
+CREATE TABLE `order` (
   `OrderID` int(10) UNSIGNED NOT NULL,
   `CustomerID` int(10) NOT NULL,
   `Date` date NOT NULL,
@@ -82,22 +81,22 @@ CREATE TABLE `Order` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `Order`
+-- Dumping data for table `order`
 --
 
-INSERT INTO `Order` (`OrderID`, `CustomerID`, `Date`, `Total`, `Note`) VALUES
-(0, 1, '2021-08-15', 150000, 'Use environmental protection bags'),
-(1, 2, '2021-08-16', 235000, ''),
-(2, 3, '2021-08-16', 65000, 'Need fast delivery'),
-(3, 3, '2021-08-17', 80000, '');
+INSERT INTO `order` (`OrderID`, `CustomerID`, `Date`, `Total`, `Note`) VALUES
+(1, 1, '2021-08-15', 150000, 'Use environmental protection bags'),
+(2, 2, '2021-08-16', 235000, 'Hello'),
+(3, 3, '2021-08-16', 65000, 'Need fast delivery'),
+(4, 3, '2021-08-17', 80000, '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `OrderDetail`
+-- Table structure for table `orderdetail`
 --
 
-CREATE TABLE `OrderDetail` (
+CREATE TABLE `orderdetail` (
   `OrderID` int(10) UNSIGNED NOT NULL,
   `VegetableID` int(10) NOT NULL,
   `Quantity` tinyint(4) NOT NULL,
@@ -105,26 +104,26 @@ CREATE TABLE `OrderDetail` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `OrderDetail`
+-- Dumping data for table `orderdetail`
 --
 
-INSERT INTO `OrderDetail` (`OrderID`, `VegetableID`, `Quantity`, `Price`) VALUES
-(0, 1, 1, 30000),
-(0, 8, 1, 120000),
-(1, 4, 1, 80000),
-(1, 2, 1, 35000),
-(1, 3, 1, 150000),
-(2, 5, 1, 35000),
-(2, 7, 2, 30000),
-(3, 6, 2, 80000);
+INSERT INTO `orderdetail` (`OrderID`, `VegetableID`, `Quantity`, `Price`) VALUES
+(1, 1, 1, 30000),
+(1, 8, 1, 120000),
+(2, 2, 1, 35000),
+(2, 3, 1, 150000),
+(2, 4, 1, 80000),
+(3, 5, 1, 35000),
+(3, 7, 2, 30000),
+(4, 6, 2, 80000);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Vegetable`
+-- Table structure for table `vegetable`
 --
 
-CREATE TABLE `Vegetable` (
+CREATE TABLE `vegetable` (
   `VegetableID` int(10) NOT NULL,
   `CatagoryID` int(10) NOT NULL,
   `VegetableName` varchar(30) NOT NULL,
@@ -135,10 +134,10 @@ CREATE TABLE `Vegetable` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `Vegetable`
+-- Dumping data for table `vegetable`
 --
 
-INSERT INTO `Vegetable` (`VegetableID`, `CatagoryID`, `VegetableName`, `Unit`, `Amount`, `Image`, `Price`) VALUES
+INSERT INTO `vegetable` (`VegetableID`, `CatagoryID`, `VegetableName`, `Unit`, `Amount`, `Image`, `Price`) VALUES
 (1, 1, 'Tomato', 'kg', 100, 'images/tomato.jpg', 30000),
 (2, 1, 'potato', 'kg', 150, 'images/potato.jpg', 35000),
 (3, 1, 'Apple', 'bag', 50, 'images/apple.jpg', 150000),
@@ -153,60 +152,71 @@ INSERT INTO `Vegetable` (`VegetableID`, `CatagoryID`, `VegetableName`, `Unit`, `
 --
 
 --
--- Indexes for table `Category`
+-- Indexes for table `category`
 --
-ALTER TABLE `Category`
+ALTER TABLE `category`
   ADD PRIMARY KEY (`CatagoryID`);
 
 --
--- Indexes for table `Customers`
+-- Indexes for table `customers`
 --
-ALTER TABLE `Customers`
+ALTER TABLE `customers`
   ADD PRIMARY KEY (`CustomerID`);
 
 --
--- Indexes for table `Order`
+-- Indexes for table `order`
 --
-ALTER TABLE `Order`
+ALTER TABLE `order`
   ADD PRIMARY KEY (`OrderID`),
   ADD KEY `CustomerID` (`CustomerID`);
 
 --
--- Indexes for table `OrderDetail`
+-- Indexes for table `orderdetail`
 --
-ALTER TABLE `OrderDetail`
+ALTER TABLE `orderdetail`
+  ADD PRIMARY KEY (`OrderID`,`VegetableID`),
   ADD KEY `OrderID` (`OrderID`),
   ADD KEY `VegetableID` (`VegetableID`);
 
 --
--- Indexes for table `Vegetable`
+-- Indexes for table `vegetable`
 --
-ALTER TABLE `Vegetable`
+ALTER TABLE `vegetable`
   ADD PRIMARY KEY (`VegetableID`),
   ADD KEY `CatagoryID` (`CatagoryID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `OrderID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `Category`
+-- Constraints for table `category`
 --
-ALTER TABLE `Category`
-  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`CatagoryID`) REFERENCES `Vegetable` (`CatagoryID`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `category`
+  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`CatagoryID`) REFERENCES `vegetable` (`CatagoryID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `Order`
+-- Constraints for table `order`
 --
-ALTER TABLE `Order`
-  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `Customers` (`CustomerID`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`CustomerID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `OrderDetail`
+-- Constraints for table `orderdetail`
 --
-ALTER TABLE `OrderDetail`
-  ADD CONSTRAINT `orderdetail_ibfk_1` FOREIGN KEY (`VegetableID`) REFERENCES `Vegetable` (`VegetableID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `orderdetail_ibfk_2` FOREIGN KEY (`OrderID`) REFERENCES `Order` (`OrderID`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `orderdetail`
+  ADD CONSTRAINT `orderdetail_ibfk_1` FOREIGN KEY (`VegetableID`) REFERENCES `vegetable` (`VegetableID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orderdetail_ibfk_2` FOREIGN KEY (`OrderID`) REFERENCES `order` (`OrderID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
